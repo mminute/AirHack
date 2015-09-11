d865380789b589493bc39d15b8fe167284c18818
mminute <masondjennings@gmail.com>
2015-09-11 13:35:49 -0400

    Adds AirportPresenceVerifier class to find links from the AirportURLs hash that were not scraped.

--- lib/AirportBatchFileCreator.rb
+++ lib/AirportBatchFileCreator.rb
@@ -25,8 +25,44 @@ end
 # url_array.grab_links
 # links = url_array.all_links
 
-links = ["http://www.airnav.com/airport/CZPC"]
+# links = ["http://www.airnav.com/airport/CZPC"]
 
-batch_creator = AirportBatchFileCreator.new(links)
-batch_creator.batch_create
-puts Dir["./lib/airport_files/*"].count # Return an array of all the files in a directory
+# batch_creator = AirportBatchFileCreator.new(links)
+# batch_creator.batch_create
+# puts Dir["./lib/airport_files/*"].count # Return an array of all the files in a directory
+
+# def missing_airports(links_array)
+#   files_found = Dir["./lib/airport_files/*"]
+#   [].tap do |missing_files|
+#     links_array.each do |link|
+#       if files_found
+#         "http://www.airnav.com/airport/KGEY"
+#       end
+#     end
+#   end
+# end
+
+# def airport_from_txt(txt_file)
+#   /[\d\w]+\.txt/.match(txt_file)[0][0..-5]
+# end
+
+# def airnav_prefixer(id)
+#   "http://www.airnav.com/airport/" + id
+# end
+
+# def files_to_url(arr)
+#   arr.map do |file_name|
+#     ident = airport_from_txt(file_name)
+#     airnav_prefixer(ident)
+#   end
+# end
+
+# p missing_airports(links)
+
+# p airport_from_txt("./lib/airport_files/Z95.txt")
+# p airport_from_txt("./lib/airport_files/KSMX.txt")
+
+# p airnav_prefixer("KSMX")
+
+# files_found = Dir["./lib/airport_files/*"]
+# p files_to_url( files_found )
