a1b0b4fc80e784941ec77bd6f17a7fed35b39612
mminute <masondjennings@gmail.com>
2015-09-14 16:24:10 -0400

    Cleans up BatchFileCreator

--- lib/AirportBatchFileCreator.rb
+++ lib/AirportBatchFileCreator.rb
@@ -1,4 +1,5 @@
 require 'timeout'
+require 'pry'
 require_relative 'AirportURLS'
 require_relative 'LinksFromHash'
 require_relative 'AirportInfoScrapper'
@@ -18,16 +19,14 @@ class AirportBatchFileCreator
                         !existing_files.include?(ident)
                       end
 
-    urls.each do |url|
+    not_yet_grabbed.each do |url|
       begin
-        Timeout.timeout( 6 ) do
+        Timeout.timeout( 30 ) do
           airport = AirportInfoScraper.new(url)
           writer = AirportFileWriter.new(airport)
           writer.write_new_file
         end
-        sleep(3)
-      rescue Timeout::Error
-        next
+        sleep(5)
       rescue
         next
       end
@@ -35,28 +34,9 @@ class AirportBatchFileCreator
   end
 end
 
-# TESTING HOW LONG 4 PAGES TAKE TO DETERMINE THE LENGTH OF THE TIMEOUT------------
-# links = ["http://www.airnav.com/airport/CZPC", "http://www.airnav.com/airport/kdxr", "http://www.airnav.com/airport/kpne", "http://www.airnav.com/airport/kbos"]
-
-# start_time = Time.now
-# batch_creator = AirportBatchFileCreator.new(links)
-# batch_creator.batch_create
-# end_time = Time.now
-# elapsed = end_time - start_time
-# puts "#{elapsed} second to complete #{links.count} pages." #8.481718 second to complete 4 pages.
-# --------------------------------------------------------------------------------
-
-# puts Dir["./lib/airport_files/*"].count # Return an array of all the files in a directory
-
 url_array = LinksFromHash.new(AllAirportUrls)
 url_array.grab_links
 links = url_array.all_links
-# links = ["http://www.airnav.com/airport/KBOS","http://www.airnav.com/airport/CZPC","http://www.airnav.com/airport/KDXR","http://www.airnav.com/airport/KPNE","http://www.airnav.com/airport/KPHL"]
+
 batch_creator = AirportBatchFileCreator.new(links)
 p batch_creator.batch_create
-
-# batch_creator = AirportBatchFileCreator.new(["http://www.google.com"])
-# batch_creator.batch_create
-
-
-# p existing_files = Dir["./lib/airport_files/*"].map{|txt_file| /[\d\w]+\.txt/.match(txt_file)[0][0..-5].downcase}
